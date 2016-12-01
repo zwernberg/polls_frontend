279dbdb8c71a2af62af41caf628e018aad17bb14
Zach Wernberg <zawerny@gmail.com>
2016-11-30 22:15:32 -0600

    impliment findquestion

--- src/FindQuestion.vue
+++ src/FindQuestion.vue
@@ -1,14 +1,48 @@
 <template>
-  <div>
+  <form class="form-inline">
     <h3>placeholder words</h3>
-    <button class="btn btn-default">Cow</button>
-    <button class="btn btn-default">Chicken</button>
-    <button class="btn btn-default">Bull</button>
-    <button class="btn btn-default">Moose</button>
-    <button class="btn btn-default">Hawk</button>
-    <button class="btn btn-default">Lion</button>
-    <button class="btn btn-default">Rat</button>
-  </div>
+    <div class="form-group">
+      <select class="form-control" v-model="color">
+      <option>red</option>
+      <option>blue</option>
+      <option>white</option>
+      <option>yellow</option>
+      <option>green</option>
+      <option>black</option>
+      <option>orange</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+      <select class="form-control" v-model="number">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <select class="form-control" v-model="animal">
+        <option>monkey</option>
+        <option>cow</option>
+        <option>chicken</option>
+        <option>bull</option>
+        <option>moose</option>
+        <option>hawk</option>
+        <option>lion</option>
+        <option>rat</option>
+        <option>dog</option>
+        <option>cat</option>
+      </select>
+    </div>
+<button type="button" id="CreateBtn" name="CreateBtn" class="btn btn-success" v-on:click="FindQuestion()">Find Question</button>
+  </form>
 </template>
 
 <script>
@@ -16,10 +50,16 @@ export default {
   name: "FindQuestion",
   data () {
     return {
-      msg: 'Hello world!',
-      question_text: '',
-      choices: []
+      color: "red",
+      number: 1,
+      animal: "monkey"
 
+    }
+  },
+    methods: {
+    FindQuestion:function(){
+      var query = this.color + this.number.toString() + this.animal
+     this.$router.push({ path: 'get/' + query, params: { slug: query }})
     }
   }
 }
